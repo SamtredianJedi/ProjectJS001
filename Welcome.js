404c65aae3d78b05d30c2e0d741078a546d716f3
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2022-12-06 22:44:39 +0400

    upd

--- Welcome.js
+++ Welcome.js
@@ -1,1 +1,16 @@
-console.log('xoar gadaob chemi');
+/*
+let myName
+
+console.log(myName)
+
+myName = 'Rezo'
+
+console.log(myName)
+
+console.log(console.log(10))
+
+*/
+
+const myName = 'Rezo'
+
+console.log(myName)
