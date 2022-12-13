339f40dfe9a101d4a6d487e0d88e54602c920400
SamtredianJedi <104278076+SamtredianJedi@users.noreply.github.com>
2022-12-13 17:21:34 +0400

    update

---
+++ HeadFirst/src/Welcome.js
@@ -0,0 +1,33 @@
+/*
+let myName
+
+console.log(myName)
+
+myName = 'Rezo'
+
+console.log(myName)
+
+console.log(console.log(10))
+
+*/
+
+/*
+
+const myName = 'Rezo'
+
+
+
+console.log(myName)
+
+*/
+
+const objectA = {
+    a: 10,
+    b: true
+}
+
+const copyOfA = objectA
+
+copyOfA.c = 'abc'
+
+console.log(copyOfA)
